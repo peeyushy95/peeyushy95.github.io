43b138be31fd7ba95dd13a954308f17be80f38a0
Peeyush Yadav <peeyushy95@gmail.com>
2015-07-17 16:32:16 +0530

    new

--- Competitive/js/loadData.js
+++ Competitive/js/loadData.js
@@ -1,19 +1,28 @@
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
+function readTextFile(file){
+	var lines=""
+	var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
                 var allText = rawFile.responseText;
-                text = allText.replace(/\r?\n/g, '<br />');
-                document.getElementById('gg').innerHTML= text;
-                
+                lines = allText.split('\n');               
             }
         }
     }
     rawFile.send(null);
-}
+	
+	
+	
+    var output = document.getElementById('output');
+	
+	for(var line = 0; line < lines.length; line++){
+			 
+        if(!document.getElementById('timedrpact'+line)){
+            var ele = document.createElement("li");
+            ele.innerHTML="<i class='fa-li fa fa-spinner fa-spin'></i> "+lines[line];
+            output.appendChild(ele);
+            
+        }
+    }
+};
